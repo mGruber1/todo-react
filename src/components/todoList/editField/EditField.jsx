2f18ff5f0aaa24e1283b85b042ff898295ccd14b
Marcus Gruber <marcus-gruber@gmx.at>
2024-09-27 11:36:41 +0200

    feat: removed e.target.value and used reacts useRef to select dom-elements

--- src/components/todoList/editField/EditField.jsx
+++ src/components/todoList/editField/EditField.jsx
@@ -1,8 +1,11 @@
+import { useRef } from "react";
 import { Form } from "react-bootstrap";
 
 export const EditField = (props) => {
-  const handleChange = (e) => {
-    const newTodoTitle = e.target.value;
+  const formInputRef = useRef();
+
+  const handleChange = () => {
+    const newTodoTitle = formInputRef.current.value;
     props.setNewTodoTitle(newTodoTitle);
   };
 
@@ -12,6 +15,7 @@ export const EditField = (props) => {
         <Form.Label>Change Todo</Form.Label>
         <Form.Control
           type="text"
+          ref={formInputRef}
           placeholder="New Todo Name"
           value={props.newTodoTitle}
           onChange={(event) => {
--- src/components/todoList/inputField/InputField.jsx
+++ src/components/todoList/inputField/InputField.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Button, Form } from "react-bootstrap";
 
 import { v4 as uuid4 } from "uuid";
@@ -6,12 +6,16 @@ import { v4 as uuid4 } from "uuid";
 export const InputField = (props) => {
   const [todoTitle, setTodoTitle] = useState("");
   const [todoDescription, setTodoDescription] = useState("");
-  const handleTodoTitleChange = (event) => {
-    setTodoTitle(event.target.value);
+
+  const todoTitleRef = useRef();
+  const todoDescriptionRef = useRef();
+
+  const handleTodoTitleChange = () => {
+    setTodoTitle(todoTitleRef.current.value);
   };
 
-  const handleDescriptionChange = (event) => {
-    setTodoDescription(event.target.value);
+  const handleDescriptionChange = () => {
+    setTodoDescription(todoDescriptionRef.current.value);
   };
   return (
     <Form className="w-75 d-flex justify-content-center">
@@ -21,6 +25,7 @@ export const InputField = (props) => {
         placeholder="Todo..."
         aria-label="Todo..."
         aria-describedby="button-addon2"
+        ref={todoTitleRef}
         value={todoTitle}
         onChange={handleTodoTitleChange}
       />
@@ -30,6 +35,7 @@ export const InputField = (props) => {
         placeholder="Description..."
         aria-label="Description..."
         aria-describedby="button-addon2"
+        ref={todoDescriptionRef}
         value={todoDescription}
         onChange={handleDescriptionChange}
       />
